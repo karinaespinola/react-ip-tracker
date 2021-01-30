98ffc17dc536b04d82fc5c71e0ff0200783e2ed6
Karina Espinola <k.espinolah@gmail.com>
2021-01-29 20:50:30 -0600

    Add styles to the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as SearchIcon } from '../img/icon-arrow.svg';
 
 const SearchBar = () => {
     return (
         <div className="d-flex">
             <input type="text" name="search" id="search" className="searchbar" />
+            <button className="btn-search">
+                <SearchIcon/>
+            </button>
         </div>
     )
 }
