5a439a45bc23544b92a63d3eb1f56f375c2fb93e
Karina Espinola <k.espinolah@gmail.com>
2021-04-09 12:54:45 -0600

    Add the Map component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './scss/style.scss';
 import Heading from './components/Heading';
+import Map from './components/Map';
 
 function App() {
   const { REACT_APP_MY_ENV } = process.env;
   return (
     <>
       <Heading />
+      <Map />
     </>    
   );
 }
