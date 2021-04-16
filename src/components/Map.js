ea9422aa5b5cc311eae3fbd81484cd31e154f575
Karina Espinola <k.espinolah@gmail.com>
2021-04-15 18:29:00 -0600

    Update map when the IP changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import {IpDataContext} from '../context/IpDataProvider';
 
 const Map = () => {
-   const {ipData} = React.useContext(IpDataContext);
+    const {ipData} = React.useContext(IpDataContext);
+
+    function UpdateMap() {
+        const map = useMap();
+        map.flyTo({
+            lat: ipData.latitude,
+            lng: ipData.longitude
+        },
+        map.getZoom());
+        return null;
+    }
 
     return (
         <MapContainer 
@@ -12,6 +22,7 @@ const Map = () => {
         scrollWheelZoom={false} 
         className="map"
         >
+            <UpdateMap />
             <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
